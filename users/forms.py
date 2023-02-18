e993c1fc1c2471c75767c1869152288b57ebd135
jck-bit <jckkinyanjui@gmail.com>
2023-02-18 23:18:23 +0300

    users can now post images

--- users/forms.py
+++ users/forms.py
@@ -20,4 +20,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['bio','image']
