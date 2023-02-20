ee767381061397f6c952f2a4fd21e09f71400c24
jck-bit <jckkinyanjui@gmail.com>
2023-02-20 16:59:38 +0300

    changed media url

--- another_social_app/settings.py
+++ another_social_app/settings.py
@@ -129,15 +129,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
