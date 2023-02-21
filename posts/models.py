55c1851b5ac076fa8032d3c223661070b0bcd881
jck-bit <jckkinyanjui@gmail.com>
2023-02-21 14:51:18 +0300

    changed the media root storage to AWS

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-# from PIL import Image
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,14 +11,3 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.caption
-
-    # def save(self, **kwargs):
-    #     super().save()
-
-    #     if self.image:
-    #         img = Image.open(self.image.path)
-
-    #         if img.height > 300 or img.width >300:
-    #             output_size = (300, 300)
-    #             img.thumbnail(output_size)
-    #             img.save(self.image.path)
--- users/models.py
+++ users/models.py
@@ -8,14 +8,3 @@ class Profile(models.Model):
 
     def __str__(self) -> str:
         return f"{self.user.username} profile"
-    
-
-    # def save(self, **kwargs):
-    #     super().save()
-
-    #     img = Image.open(self.image.path)
-
-    #     if img.height > 300 or img.width >300:
-    #         output_size = (300, 300)
-    #         img.thumbnail(output_size)
-    #         img.save(self.image.path)
