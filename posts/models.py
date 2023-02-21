fb290c14974c5cbd8622b86d95629e5d4ac857de
jck-bit <jckkinyanjui@gmail.com>
2023-02-21 14:38:38 +0300

    changed the media root storage to AWS

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from PIL import Image
+# from PIL import Image
 
 class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -13,13 +13,13 @@ class Post(models.Model):
     def __str__(self) -> str:
         return self.caption
 
-    def save(self, **kwargs):
-        super().save()
+    # def save(self, **kwargs):
+    #     super().save()
 
-        if self.image:
-            img = Image.open(self.image.path)
+    #     if self.image:
+    #         img = Image.open(self.image.path)
 
-            if img.height > 300 or img.width >300:
-                output_size = (300, 300)
-                img.thumbnail(output_size)
-                img.save(self.image.path)
+    #         if img.height > 300 or img.width >300:
+    #             output_size = (300, 300)
+    #             img.thumbnail(output_size)
+    #             img.save(self.image.path)
--- users/models.py
+++ users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,12 +10,12 @@ class Profile(models.Model):
         return f"{self.user.username} profile"
     
 
-    def save(self, **kwargs):
-        super().save()
+    # def save(self, **kwargs):
+    #     super().save()
 
-        img = Image.open(self.image.path)
+    #     img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width >300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width >300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
