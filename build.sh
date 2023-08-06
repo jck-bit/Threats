4ac37eb73b34b38fcd0f0255384a17e099ab477c
jck-bit <jckkinyanjui@gmail.com>
2023-08-06 23:25:58 +0300

    updated python version

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Make Migrations..."
-python3.8.10 manage.py makemigrations --noinput
-python3.8.10 manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.8.10 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
