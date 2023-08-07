56534c1d0bf665cfa8ef83dd95fdfea5ea31a4eb
jck-bit <jckkinyanjui@gmail.com>
2023-08-07 16:48:27 +0300

    edited the build.sh file

--- another_social_app/urls.py
+++ another_social_app/urls.py
@@ -36,5 +36,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
-echo "Make Migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
 
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+# activate virtual environment
+# python3.9 -m venv venv
+# source venv/bin/activate
+# pip install -r requirements.txt
+
+python3.9 manage.py collectstatic 
