4165fdd7d32bfd410158ef635806eb9597922d59
jck-bit <jckkinyanjui@gmail.com>
2023-08-13 23:56:53 +0300

    added dockerfile and nginx to deploy to amazon ecs

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python3 manage.py makemigrations
+python3 manage.py migrate --no-input
+
+gunicorn another_social_app.wsgi:application --bind 0.0.0.0:8000 &
+
+unlink /etc/nginx/sites-enabled/default
+nginx -g 'daemon off;'
