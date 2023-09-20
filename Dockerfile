3460eb7e7ac72034f33711d7a3b75a5d9f0c73c6
jck-bit <jckkinyanjui@gmail.com>
2023-09-20 13:42:59 +0300

    added an images volume to prevent overide when deploying

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,15 @@ WORKDIR /code
 COPY requirements.txt /code/
 COPY ./scripts /scripts
 
+
+
 RUN pip install -r requirements.txt && \
     mkdir -p /vol/web/static && \
     mkdir -p /vol/web/media && \
     chmod -R 755 /vol && \
     chmod -R +x /scripts
+
+COPY default.jpeg /code/media/
 
 
 COPY . /code/
--- users/signals.py
+++ users/signals.py
@@ -2,12 +2,17 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from shutil import copyfile
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        profile, _ = Profile.objects.get_or_create(user=instance)
+        if not profile.image:
+            default_image_path = '/default_media/default.jpeg'
+            user_media_path = profile.image.path
+            copyfile(default_image_path, user_media_path)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, created, **kwargs):
